8e85ae3a808a22389e00284c21807542c58810fd
Xeadriel <ogulcanasik@web.de>
2024-07-29 15:16:02 +0200

    testing configs

--- RepositoryInit.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Main.Users (
-	ID int NOT NULL AUTO_INCREMENT UNIQUE,
-    username varchar(255) NOT NULL UNIQUE,
-    pass varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-	PRIMARY KEY (ID)
-); 
