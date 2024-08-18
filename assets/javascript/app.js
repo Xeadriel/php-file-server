b4ef455c6893872eb3820902fa0a7d129e168c95
mokkez <sakazethegoldenlion@gmail.com>
2024-08-18 20:41:48 +0200

    dark mode added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,22 @@
 import './bootstrap.js';
+import '../styles/app.css';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+
+const themeBtn = document.getElementById("themeChange");
+themeBtn.onclick = () => {
+  themeBtn.classList.toggle("bx-sun");
+  if (themeBtn.classList.contains("bx-sun")) {
+    document.body.classList.add("changeTheme");
+  } else {
+    document.body.classList.remove("changeTheme");
+  }
+}
+
 
 
