04764266388db1a9c9320b1316a401a5b542268b
mokkez <sakazethegoldenlion@gmail.com>
2024-08-22 19:53:45 +0200

    dark mode persist across pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,16 +7,41 @@ import '../styles/app.css';
  * which should already be in your base.html.twig.
  */
 
-
-const themeBtn = document.getElementById("themeChange");
-themeBtn.onclick = () => {
-  themeBtn.classList.toggle("bx-sun");
-  if (themeBtn.classList.contains("bx-sun")) {
-    document.body.classList.add("changeTheme");
-  } else {
-    document.body.classList.remove("changeTheme");
+  var theme = localStorage.getItem('theme');
+  if(theme === null){
+    localStorage.setItem('theme', 'light');
   }
-}
+
+  const themeBtn = document.getElementById("themeChange");
+  
+  theme = localStorage.getItem('theme');
+  if(theme === 'dark'){
+    document.body.classList.add("changeTheme");
+    themeBtn.classList.add("bx-sun");
+  
+  }
+  else {
+    document.body.classList.remove("changeTheme");
+    themeBtn.classList.remove("bx-sun");
+  
+  }
+
+  themeBtn.onclick = () => {
+    theme = localStorage.getItem('theme');
+    if(theme !== 'dark'){
+      localStorage.setItem('theme', 'dark');
+      theme = localStorage.getItem('theme');
+      document.body.classList.add("changeTheme");
+      themeBtn.classList.toggle("bx-sun");
+    }
+    else {
+      localStorage.setItem('theme', 'light');
+      theme = localStorage.getItem('theme');
+      document.body.classList.remove("changeTheme");
+      themeBtn.classList.toggle("bx-sun");
+    }
+  }
+
 
 
 
